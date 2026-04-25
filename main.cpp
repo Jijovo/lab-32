18e01d7093d3e75765c1e76eb10ac8489248a8bc
Jijovo <jijovo77@gmail.com>
2026-04-24 17:59:44 -0700

    milestone 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 //Ezzat Mohamadein | ComSc 210 | Lab 32
 #include <iostream>
 #include <deque>
-#include <Car.h>
+#include "Car.h"
+using namespace std;
+
+//print queue function prototype
+void printQueue(std::deque<Car> q);
 
 int main() {
+    //define queue of cars and populate with 2 cars
+    const int START = 2;
+    deque<Car> carQueue;
+    for (int i = 0; i < START; i++) {
+        carQueue.push_back(Car());
+    }
+
+    //print queue for testing
+    cout << "Initial queue:" << endl;
+    printQueue(carQueue);
 
     return 0;
+}
+
+//print queue function definition
+void printQueue(std::deque<Car> q) {
+    for (Car c : q) {
+        c.print();
+    }
 }
