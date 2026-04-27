413d57b7896b30e6bc1c094a6790ccb1a3f8dac8
Jijovo <jijovo77@gmail.com>
2026-04-26 17:17:43 -0700

    milestone 2(lab33)

--- main.cpp
+++ main.cpp
@@ -1,53 +1,46 @@
 //Ezzat Mohamadein | ComSc 210 | Lab 33
 #include <iostream>
 #include <deque>
+#include <array>
 #include "Car.h"
 using namespace std;
 
 //print queue function prototype
 void printQueue(std::deque<Car> q);
+//print queue function prototype for array of queues
+void printQueues(array<deque<Car>, 4> q);
 //define constants
 const int START = 2;
 const int PAY = 46;
 const int JOIN = 39;
 const int SHIFT = 15;
+const int PERIODS = 20;
 
 int main() {
     //randomise seed for random number generation using current time
     srand(time(0));
-    //define queue of cars and populate with 2 cars
-    deque<Car> carQueue;
-    for (int i = 0; i < START; i++) {
-        carQueue.push_back(Car());
+    //define array containing 4 queues of cars and populate with 2 cars each
+    array<deque<Car>, 4> carQueues;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < START; j++) {
+            carQueues[i].push_back(Car());
+        }
     }
 
     //print queue for testing
-    cout << "Initial queue:" << endl;
-    printQueue(carQueue);
+    cout << "Initial queues:" << endl;
+    printQueues(carQueues);
 
-    //while loop that runs until queue is empty
+    //test popping,pushing, and printing
+    cout << endl << "Testing queue operations:" << endl;
+    carQueues[0].pop_front();
+    carQueues[1].push_back(Car());
+    printQueue(carQueues[0]);
+    printQueue(carQueues[1]);
+    //while loop that runs until 20 time periods have passed
     int t = 1;
-    while (!carQueue.empty()) {
-        //print current time period
-        cout << endl << "Time:" << t << endl;
-        //55% chance of first car in the queue leaving, 45% chance of a new car arriving at the back
-        if (rand() % 100 < 55) {
-            cout << "Car paid: ";
-            carQueue.front().print();
-            carQueue.pop_front();
-        }
-        else {
-            carQueue.push_back(Car());
-            cout << "New car arrived: ";
-            carQueue.back().print();
-        }
-        //print current queue
-        cout << "Current queue:" << endl;
-        printQueue(carQueue);
-        //increment time period
-        t++;
-            
-    }
+    
+    
 
     return 0;
 }
@@ -59,5 +52,12 @@ void printQueue(std::deque<Car> q) {
     }
     if (q.empty()) {
         cout << "[Empty]" << endl;
+    }
+}
+//print queue function definition for array of queues
+void printQueues(array<deque<Car>, 4> q) {
+    for (int i = 0; i < q.size(); i++) {
+        cout << "Queue " << i + 1 << ":" << endl;
+        printQueue(q[i]);
     }
 }
